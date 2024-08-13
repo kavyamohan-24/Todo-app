df8d16d81c050283fbcf29ae5a4a5347174ffdc1
kavyamohan-24 <kavyamohan2005@gmail.com>
2024-08-13 23:00:25 +0530

    to do app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import AddTodo from './components/AddTodo';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SearchTodo from './components/SearchTodo';
+import ViewAll from './components/ViewallTodo';
+
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
     <Routes>
       <Route path="/" element={<AddTodo/>}/>
       <Route path="/search" element={<SearchTodo/>}/>
+     <Route path="/viewall" element={<ViewAll/>}/>
+      
     </Routes>
     </BrowserRouter>
   );
