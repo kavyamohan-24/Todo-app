fe45c8dd4a2c08f9aefcc5c19b415e1d3c1ce633
kavyamohan-24 <kavyamohan2005@gmail.com>
2024-08-13 22:26:18 +0530

    to do app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import AddTodo from './components/AddTodo';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SearchTodo from './components/SearchTodo';
 
 function App() {
   return (
-    <div>
-      <AddTodo/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<AddTodo/>}/>
+      <Route path="/search" element={<SearchTodo/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
